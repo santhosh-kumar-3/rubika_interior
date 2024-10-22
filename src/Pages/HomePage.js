b9b4457946c0fa3617ca9677721984bad151e880
santhosh kumar <santhoshkmb123sk@gmail.com>
2024-10-22 14:09:26 +0530

    All Over Going to Deploy

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -21,7 +21,7 @@ const HomePage = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000); // Set the duration you want for loading
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
